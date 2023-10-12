692824cadae75a51ca8ca5f6ba3029733cf9a72f
joshua-8 <joshua_phelps@brown.edu>
2023-10-11 22:18:25 -0400

    driving with field oriented control

--- RCMv2/RCMv2.ino
+++ RCMv2/RCMv2.ino
@@ -2,56 +2,85 @@
 //   https://github.com/rcmgames/RCMv2
 //   for information about the electronics, see the link at the top of this page: https://github.com/RCMgames
 #include "rcm.h" //defines pins
+#include <Adafruit_BNO08x.h> //https://github.com/adafruit/Adafruit_BNO08x v1.2.3
 #include <ESP32_easy_wifi_data.h> //https://github.com/joshua-8/ESP32_easy_wifi_data >=v1.0.0
 #include <JMotor.h> //https://github.com/joshua-8/JMotor
 
-const int dacUnitsPerVolt = 380; // increasing this number decreases the calculated voltage
+const int dacUnitsPerVolt = 413; // increasing this number decreases the calculated voltage
 const int encoderTicksPerRev = 625;
 float wheelCir = (0.1 * PI);
-JTwoDTransform robotToWheelScalar = { 1 / wheelCir, 1 / wheelCir, 1 / wheelCir }; // adjust until it converts robot speed in your chosen units to wheel units (increasing numbers makes robot faster)
-
+JTwoDTransform robotToWheelScalar = { 1 / wheelCir, 1 / wheelCir, 1.35 / ((float)TWO_PI) / wheelCir }; // adjust until it converts robot speed in your chosen units to wheel units (increasing numbers makes robot faster)
+//TODO: calibrate robot to wheel scalar
 JVoltageCompMeasure<10> voltageComp = JVoltageCompMeasure<10>(batMonitorPin, dacUnitsPerVolt);
 JMotorDriverEsp32L293 flMotorDriver = JMotorDriverEsp32L293(portA, true, false, false, 8000, 12);
 JMotorDriverEsp32L293 frMotorDriver = JMotorDriverEsp32L293(portB, true, false, false, 8000, 12);
 JMotorDriverEsp32L293 blMotorDriver = JMotorDriverEsp32L293(portC, true, false, false, 8000, 12);
 JMotorDriverEsp32L293 brMotorDriver = JMotorDriverEsp32L293(portD, true, false, false, 8000, 12);
-JEncoderSingleAttachInterrupt flEncoder = JEncoderSingleAttachInterrupt(inport1, 1.0 / encoderTicksPerRev, false, 200000, 25, FALLING);
-JEncoderSingleAttachInterrupt frEncoder = JEncoderSingleAttachInterrupt(inport2, 1.0 / encoderTicksPerRev, false, 200000, 25, FALLING);
-JEncoderSingleAttachInterrupt blEncoder = JEncoderSingleAttachInterrupt(inport3, 1.0 / encoderTicksPerRev, false, 200000, 25, FALLING);
-JEncoderSingleAttachInterrupt brEncoder = JEncoderSingleAttachInterrupt(port1Pin, 1.0 / encoderTicksPerRev, false, 200000, 25, FALLING);
-JMotorCompStandardConfig flMotorConfig = JMotorCompStandardConfig(3, .55, 5, 1.60, 6.75, 2.75, 100);
-JMotorCompStandardConfig frMotorConfig = JMotorCompStandardConfig(3, .55, 5, 1.60, 6.75, 2.75, 100);
-JMotorCompStandardConfig blMotorConfig = JMotorCompStandardConfig(3, .55, 5, 1.60, 6.75, 2.75, 100);
-JMotorCompStandardConfig brMotorConfig = JMotorCompStandardConfig(3, .55, 5, 1.60, 6.75, 2.75, 100);
-JMotorCompStandard flMotorCompensator = JMotorCompStandard(voltageComp, flMotorConfig, 1.0);
-JMotorCompStandard frMotorCompensator = JMotorCompStandard(voltageComp, frMotorConfig, 1.0);
-JMotorCompStandard blMotorCompensator = JMotorCompStandard(voltageComp, blMotorConfig, 1.0);
-JMotorCompStandard brMotorCompensator = JMotorCompStandard(voltageComp, brMotorConfig, 1.0);
-JControlLoopBasic flCtrlLoop = JControlLoopBasic(7);
-JControlLoopBasic frCtrlLoop = JControlLoopBasic(7);
-JControlLoopBasic blCtrlLoop = JControlLoopBasic(7);
-JControlLoopBasic brCtrlLoop = JControlLoopBasic(7);
-JMotorControllerClosed flMotor = JMotorControllerClosed(flMotorDriver, flMotorCompensator, flEncoder, flCtrlLoop);
-JMotorControllerClosed frMotor = JMotorControllerClosed(frMotorDriver, frMotorCompensator, frEncoder, frCtrlLoop);
-JMotorControllerClosed blMotor = JMotorControllerClosed(blMotorDriver, blMotorCompensator, blEncoder, blCtrlLoop);
-JMotorControllerClosed brMotor = JMotorControllerClosed(brMotorDriver, brMotorCompensator, brEncoder, brCtrlLoop);
+JEncoderSingleAttachInterrupt flEncoder = JEncoderSingleAttachInterrupt(inport1, 1.0 / encoderTicksPerRev, false, 20000, 200, FALLING);
+JEncoderSingleAttachInterrupt frEncoder = JEncoderSingleAttachInterrupt(inport2, 1.0 / encoderTicksPerRev, false, 20000, 200, FALLING);
+JEncoderSingleAttachInterrupt blEncoder = JEncoderSingleAttachInterrupt(inport3, 1.0 / encoderTicksPerRev, false, 20000, 200, FALLING);
+JEncoderSingleAttachInterrupt brEncoder = JEncoderSingleAttachInterrupt(port1Pin, 1.0 / encoderTicksPerRev, false, 20000, 200, FALLING);
+JMotorCompBasic motorCompensator = JMotorCompBasic(voltageComp, 1.7, 0.5); // volts per rps, min rps
+JControlLoopBasic flCtrlLoop = JControlLoopBasic(10, 400);
+JControlLoopBasic frCtrlLoop = JControlLoopBasic(10, 400);
+JControlLoopBasic blCtrlLoop = JControlLoopBasic(10, 400);
+JControlLoopBasic brCtrlLoop = JControlLoopBasic(10, 400);
+JMotorControllerClosed flMotor = JMotorControllerClosed(flMotorDriver, motorCompensator, flEncoder, flCtrlLoop, INFINITY, INFINITY, 0.1);
+JMotorControllerClosed frMotor = JMotorControllerClosed(frMotorDriver, motorCompensator, frEncoder, frCtrlLoop, INFINITY, INFINITY, 0.1);
+JMotorControllerClosed blMotor = JMotorControllerClosed(blMotorDriver, motorCompensator, blEncoder, blCtrlLoop, INFINITY, INFINITY, 0.1);
+JMotorControllerClosed brMotor = JMotorControllerClosed(brMotorDriver, motorCompensator, brEncoder, brCtrlLoop, INFINITY, INFINITY, 0.1);
 JDrivetrainMecanum drivetrain = JDrivetrainMecanum(frMotor, flMotor, blMotor, brMotor, robotToWheelScalar); // drivetrain converts from robot speed to motor speeds
-JDrivetrainControllerBasic drivetrainController = JDrivetrainControllerBasic(drivetrain, { INFINITY, INFINITY, INFINITY }, { 3, 3, 3 }, { 0.01, 0.01, 0.01 });
+JDrivetrainFieldOriented drivetrainFO = JDrivetrainFieldOriented(drivetrain);
+JDrivetrainControllerBasic drivetrainController = JDrivetrainControllerBasic(drivetrainFO, { INFINITY, INFINITY, INFINITY }, { 5, 5, 5 }, { 0.05, 0.05, 0.5 }, false);
 
 JTwoDTransform driverInput = JTwoDTransform();
+float heading = 0;
+float headingOffset = 0;
+
+struct euler_t {
+    float yaw;
+    float pitch;
+    float roll;
+} ypr;
+Adafruit_BNO08x bno08x(-1);
+sh2_SensorValue_t sensorValue;
+void quaternionToEuler(float qr, float qi, float qj, float qk, euler_t* ypr)
+{
+    float sqr = sq(qr);
+    float sqi = sq(qi);
+    float sqj = sq(qj);
+    float sqk = sq(qk);
+    ypr->yaw = atan2(2.0 * (qi * qj + qk * qr), (sqi - sqj - sqk + sqr));
+    ypr->pitch = asin(-2.0 * (qi * qk - qj * qr) / (sqi + sqj + sqk + sqr));
+    ypr->roll = atan2(2.0 * (qj * qk + qi * qr), (-sqi - sqj + sqk + sqr));
+}
+void quaternionToEulerRV(sh2_RotationVectorWAcc_t* rotational_vector, euler_t* ypr)
+{
+    quaternionToEuler(rotational_vector->real, rotational_vector->i, rotational_vector->j, rotational_vector->k, ypr);
+}
+void setReports()
+{
+    if (!bno08x.enableReport(SH2_ARVR_STABILIZED_RV, 5000)) {
+        Serial.println("Could not enable stabilized remote vector, STOPPING PROGRAM");
+        while (1) {
+            delay(10);
+        }
+    }
+}
 
 void Enabled()
 {
     // code to run while enabled, put your main code here
-    drivetrainController.setVel(driverInput);
-
-    // drivetrainController.setDistDelta({ 0.2, 0, 0 });
+    drivetrainController.moveVel(driverInput);
 }
 
 void Enable()
 {
     // turn on outputs
+    drivetrainFO.resetDist();
+    headingOffset = heading;
     drivetrainController.enable();
+    driverInput = { 0, 0, 0 };
 }
 
 void Disable()
@@ -72,13 +101,33 @@ void PowerOn()
     flEncoder.setUpInterrupts(flEncoder_jENCODER_ISR);
     brEncoder.setUpInterrupts(brEncoder_jENCODER_ISR);
     blEncoder.setUpInterrupts(blEncoder_jENCODER_ISR);
+
+    Wire.begin(port4Pin, port5Pin); // sda,scl
+
+    if (!bno08x.begin_I2C()) {
+        Serial.println("Failed to find BNO08x chip, STOPPING PROGRAM");
+        while (1) {
+            delay(10);
+        }
+    }
+    if (!bno08x.enableReport(SH2_ARVR_STABILIZED_RV, 5000)) {
+        Serial.println("Could not enable stabilized remote vector, STOPPING PROGRAM");
+        while (1) {
+            delay(10);
+        }
+    }
 }
 
 void Always()
 {
     // always runs if void loop is running, top level JMotor run() functions should be put here
+    if (bno08x.getSensorEvent(&sensorValue)) {
+        quaternionToEulerRV(&sensorValue.un.arvrStabilizedRV, &ypr);
+    }
+    heading = ypr.yaw;
+    drivetrainFO.giveHeading(heading - headingOffset);
     drivetrainController.run();
-    delay(10);
+    delay(1);
 }
 
 void configWifi()
@@ -88,11 +137,6 @@ void configWifi()
     EWD::routerPassword = "password";
     EWD::routerPort = 25210;
     EWD::signalLossTimeout = 350;
-
-    // EWD::mode = EWD::Mode::createAP;
-    // EWD::APName = "lab4bot";
-    // EWD::APPassword = "Borton";
-    // EWD::APPort = 25210;
 }
 
 void WifiDataToParse()
@@ -107,6 +151,10 @@ void WifiDataToSend()
 {
     EWD::sendFl(voltageComp.getSupplyVoltage());
     // add data to send here: (EWD::sendBl(), EWD::sendBy(), EWD::sendIn(), EWD::sendFl())(boolean, byte, int, float)
+    EWD::sendFl(drivetrainController.getVel().x);
+    EWD::sendFl(drivetrainController.getVel().y);
+    EWD::sendFl(drivetrainController.getVel().theta);
+    EWD::sendFl(drivetrainFO.getHeading());
 }
 
 ////////////////////////////// you don't need to edit below this line ////////////////////
